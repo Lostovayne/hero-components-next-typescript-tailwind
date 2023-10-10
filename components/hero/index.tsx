464ca1a1c5aac7f4b0f387b24dfb1617afd91965
LostoVayne <moises.barrera.dev@gmail.com>
2023-10-10 15:59:57 -0300

    acabado

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -7,6 +7,11 @@ export const Hero = () => {
             <Wallpaper />
             <SectionText />
             <SectionImages />
+            <div className="absolute z-30 top-1/2 -translate-y-1/2 -left-[8.5rem] ">
+                <h3 className="text-white font-medium uppercase tracking-[8px] text-sm -rotate-90 relative before:absolute before:w-20 before:h-1 before:bg-white before:-right-28 before:top-1/2 before:-translate-y-1/2">
+                    Romanian Mountains
+                </h3>
+            </div>
         </section>
     );
 };
